4ba087838a8ed5bd51b3a72fdacb7859c4caae27
Kelly McGuffog <kellymcguffog@gmail.com>
2023-06-21 14:18:44 -0400

    styling destinations page

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return(
+        <>
+        <h1 className="title">Wanderlust</h1>
+        <h3 className="subtitle">Travel the world with ease</h3>
+        </>
+    )
+}
+
+export default Header;
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,12 @@
-import React, {useContext} from "react";
+import React, {useState, useContext} from "react";
 import { UserContext } from "../context/UserContext";
 import { Link, NavLink } from "react-router-dom";
+import Header from "./Header";
 
 function NavBar() {
     const {user, setUser} = useContext(UserContext)
+    const [isDropdown, setDropdown] = useState(false)
+    
     function handleLogoutClick() {
         fetch("/logout", { method: "DELETE" }).then((r) => {
             if (r.ok) {
@@ -12,15 +15,27 @@ function NavBar() {
         });
     }
 
+    function handleDropdown(){
+        setDropdown(isDropdown => !isDropdown)
+    }
+
     return (
+        <div className="header">
         <header>
             <Link to="/"><h3 className="navbar logo">Wanderlust</h3></Link>
             <div className="navbar">
                 {user ? (
                     <>
                         <NavLink to="/destinations">Destinations</NavLink>
-                        <NavLink to="/profile">Profile</NavLink>
-                        <button className="logout" onClick={handleLogoutClick}>Logout</button>
+                        {/* <NavLink to="/profile">Profile</NavLink> */}
+                        <div className="dropdown">
+                            <h3 onClick={handleDropdown} className="dropbtn">{user.first_name} {user.last_name} <i className="fa-solid fa-caret-down"></i></h3>
+                            <div className={isDropdown ? "dropdown-content visible" : "dropdown-content hidden"}>
+                                <Link to="/profile">Profile</Link>
+                                <hr></hr>
+                                <button className="logout" onClick={handleLogoutClick}>Logout</button>
+                            </div>
+                        </div>
                     </>
                 ) : (
                     <>
@@ -30,6 +45,8 @@ function NavBar() {
                 )}
             </div>
         </header>
+        <Header />
+        </div>
     );
 }
 
