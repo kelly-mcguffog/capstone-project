a8c0c706b07bb2bddc548328864da9a61f37a5ab
Kelly McGuffog <kellymcguffog@gmail.com>
2023-09-11 12:57:49 -0400

    updated Carrier Wave Storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,11 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  CarrierWave.configure do |config|
+    config.ignore_integrity_errors = false
+    config.ignore_processing_errors = false
+    config.ignore_download_errors = false
+  end
+  
 end
