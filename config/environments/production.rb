fa6a324eae0d3a27dbc41b5f737cc414d7de0629
Kelly McGuffog <kellymcguffog@gmail.com>
2023-11-28 20:03:03 -0500

    updated environment variable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,6 +119,8 @@ Rails.application.configure do
   CarrierWave.configure do |config|
     config.fog_credentials = {
     provider:              'AWS',                        # required
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
     use_iam_profile:       true,                         # optional, defaults to false
     region:                'us-east-1',                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
