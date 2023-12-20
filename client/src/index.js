85b52b3f43cf236ba9d29443c673ec3c9e95071d
Kelly McGuffog <kellymcguffog@gmail.com>
2023-12-20 17:28:06 -0500

    client side routing configuration for deploying on render

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 import { UserProvider } from './context/UserContext';
-// import { AllUsersProvider } from './context/AllUsersContext';
 import { DestinationsProvider } from './context/DestinationsContext';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -11,13 +10,11 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <UserProvider>
-      {/* <AllUsersProvider> */}
         <DestinationsProvider>
           <Router>
             <App />
           </Router>
         </DestinationsProvider>
-      {/* </AllUsersProvider> */}
     </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
