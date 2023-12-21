221ac45af007f7b4ba39f7cf8013134bed4c11d6
Kelly McGuffog <kellymcguffog@gmail.com>
2023-12-21 13:55:06 -0500

    set up aws default avatar image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,8 +16,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "https://wanderlust-app.s3.amazonaws.com#{ActionController::Base.helpers.asset_path("fallback/default_avatar.jpg")}"
-    
+    "https://wanderlust-app.s3.amazonaws.com#{ActionController::Base.helpers.asset_path("fallback/default.png")}"
+
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
